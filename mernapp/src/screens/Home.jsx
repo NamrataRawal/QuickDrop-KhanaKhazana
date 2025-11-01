7dd02a86f6bcb5e92be70d33c083240c8823f5bd
NamrataRawal <nnamrata494@gmail.com>
2025-11-01 11:18:00 +0000

    home page designation

--- mernapp/src/screens/Home.jsx
+++ mernapp/src/screens/Home.jsx
@@ -6,14 +6,33 @@ export default function Home() {
     return (
         <div>
             <div><Navbar /></div>
-            <div><div className="card" style={{"width": "18rem"}}>
-                <img src="..." className="card-img-top" alt="..." />
-                <div className="card-body">
-                    <h5 className="card-title">Card title</h5>
-                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
 
-                </div>
-            </div></div>
+            <div>
+                <div className="card mt-2" style={{ "width": "18rem", "height": "250px" }}>
+                    <img src="..." className="card-img-top" alt="..." />
+                    <div className="card-body">
+                        <h5 className="card-title">Card title</h5>
+                        <p className="card-text">This is  some Important text.</p>
+                        <div className='container w-100'>
+                            <select className='m-2 h-100 bg-success rounded'>
+                                {Array.from(Array(6), (e,i)=>{
+                                    return (
+                                        <option key={i+1} value={i+1}>{i+1}</option>
+                                    )
+                                })}
+                            </select>
+
+                            <select className='m-2 h-100 bg-success rounded'>
+                                <option value="half">Half</option>
+                                <option value="full">Full</option>
+                            </select>
+
+                            <div className='d-inline h-100 fs-5'>
+                                Total Price
+                            </div>
+                        </div>
+                    </div>
+                </div></div>
             <div><Footer /></div>
         </div>
     )
